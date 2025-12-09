bc30547d51b73c1928c81a74086a61f621165141
Ian Contijoch <ian.contijoch@gmail.com>
2025-12-08 19:14:46 -0500

    day04-part1

---
+++ day04/part1.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+
+    coords = {
+        (x, y): c
+        for y, line in enumerate(lines)
+        for x, c in enumerate(line)
+    }
+
+    for pos in coords:
+        if pos in coords and coords[pos] == '@':
+            cand = [adj for adj in support.adjacent_8(*pos) if adj in coords]
+            if ''.join(coords[c] for c in cand).count('@') < 4:
+                total += 1
+
+    return total
+
+
+INPUT_S = '''\
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+'''
+EXPECTED = 13
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
